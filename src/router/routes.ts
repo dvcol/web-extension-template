f839364afa0c919c968c6cec05d27f0c9f0e866e
Dinh-Van Colomban <van.colomban@gmail.com>
2024-05-27 19:28:27 +0200

    feat(svelte): create svelte template

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,19 +1,41 @@
-import type { RouteRecordRaw } from 'vue-router';
+import wrap from 'svelte-spa-router/wrap';
 
-export const routes: RouteRecordRaw[] = [
+import type { RouteDefinition, WrappedComponent } from 'svelte-spa-router';
+
+export const Route: Record<
+  string,
   {
-    path: `/`,
-    name: 'home',
-    redirect: { name: 'hello' },
-  },
-  {
-    path: `/hello`,
+    name: string;
+    path: string;
+    component: WrappedComponent;
+  }
+> = {
+  Hello: {
     name: 'hello',
-    component: () => import('../components/views/hello/HelloComponent.vue'),
+    path: '/hello',
+    component: wrap({
+      asyncComponent: () => import('~/components/hello/HelloComponent.svelte'),
+    }),
   },
-  {
-    path: `/goodbye`,
+  Goodbye: {
     name: 'goodbye',
-    component: () => import('../components/views/goodbye/GoodbyeComponent.vue'),
+    path: '/goodbye',
+    component: wrap({
+      asyncComponent: () => import('~/components/goodbye/GoodbyeComponent.svelte'),
+    }),
   },
-];
+} as const;
+
+export const routeMap = Object.values(Route).map(route => ({ name: route.name, path: route.path }));
+
+export const routeDefinition: RouteDefinition = {
+  // Home
+  '/': Route.Hello.component,
+
+  // Routes
+  [Route.Hello.path]: Route.Hello.component,
+  [Route.Goodbye.path]: Route.Goodbye.component,
+
+  // Catch-all
+  '*': Route.Hello.component,
+};
