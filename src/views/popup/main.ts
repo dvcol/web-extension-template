3f9530ab1314f602c39fb828152748de988721a8
Dinh-Van Colomban <van.colomban@gmail.com>
2024-09-02 23:08:25 +0200

    fix(popup): adds resize handler

--- src/views/popup/main.ts
+++ src/views/popup/main.ts
@@ -1,6 +1,17 @@
 import { Logger } from '~/services/logger.service';
 import defineComponent from '~/web/define-component';
 
+const resizeContainer = () => {
+  const root = document?.getElementById('app-popup');
+
+  if (!root) return;
+  root.style.width = `${window.innerWidth}px`;
+  root.style.height = `${window.innerHeight}px`;
+};
+
 defineComponent({ baseUrl: import.meta.env.VITE_BASE, view: { popup: true, web: import.meta.env.VITE_WEB ?? false } })
-  .then(() => Logger.info('Web Component defined', import.meta.env))
+  .then(() => {
+    Logger.info('Web Component defined');
+    setTimeout(resizeContainer);
+  })
   .catch(err => Logger.error('Failed to define component', err));
