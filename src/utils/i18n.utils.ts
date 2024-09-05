1c5ca91c85a567f037ebef70f31908a73b74f07c
Dinh-Van Colomban <van.colomban@gmail.com>
2024-09-05 03:59:18 +0200

    fix(i18n): prevent infinite hmr loop

--- src/utils/i18n.utils.ts
+++ src/utils/i18n.utils.ts
@@ -1,20 +1,23 @@
-import type { Locale, Locales } from '~/models/i18n.model';
+import type { Locale, Locales } from "~/models/i18n.model";
 
-import { Logger } from '~/services/logger.service';
-import { RouterService } from '~/services/router.service';
-import { useI18nStore } from '~/stores/i18n.store';
-import { useI18nTranslate } from '~/utils/browser/browser-i18n.utils';
-import { chromeI18n } from '~/utils/browser/browser.utils';
+import { Logger } from "~/services/logger.service";
+import { RouterService } from "~/services/router.service";
+import { useI18nStore } from "~/stores/i18n.store";
+import { useI18nTranslate } from "~/utils/browser/browser-i18n.utils";
+import { chromeI18n } from "~/utils/browser/browser.utils";
 
+let promise: Promise<Locales | void> | undefined;
+let hotReload = false;
 export const initLocalI18n = (baseUrl = RouterService.baseUrl) => {
   const store = useI18nStore();
-  let promise: Promise<Locales | void> | undefined;
+  if (hotReload || promise) return { store, promise };
   if (import.meta.hot) {
     Logger.debug('Listening to i18n HMR changes');
     import.meta.hot.send('fetch:i18n');
     import.meta.hot.on('update:i18n', (data: { lang: string; locale: Locale }[]) => {
       data?.forEach(({ lang, locale }) => store.addLocale(locale, lang, true));
     });
+    hotReload = true;
   } else if (!store.locales?.[store.lang]) {
     promise = fetch(new URL(`${baseUrl}_locales/${store.lang}/messages.json`, new URL(import.meta.url).origin))
       .then(r => r.json())
