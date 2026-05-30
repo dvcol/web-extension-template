dc9ece047054427799c98c9d8aa7ea1dce5f40dc
dvcolomban <dinh-van.colomban@contentsquare.com>
2026-05-30 19:44:21 +0200

    chore(ci): adds commit hook expansion

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 export default {
   '*.{js,cjs,mjs,jsx,ts,tsx,vue,svelte,json,md,yml,html,svg,xml}': ['eslint --fix'],
   '*.{css,scss,less,vue,svelte}': ['stylelint --fix'],
+  '*.svelte': ['pnpm run check:svelte'],
 };
